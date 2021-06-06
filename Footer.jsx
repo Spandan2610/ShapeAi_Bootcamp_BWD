b9544103ba75c2c13cb4a6f1d9d39846bc4f383e
Spandan2610 <76774332+Spandan2610@users.noreply.github.com>
2021-06-07 00:46:29 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var year = new Date().getFullYear();
+
+  return (
+    <div>
+      <footer>Copyright @ {year} </footer>
+    </div>
+  );
+}
+export default Footer;
